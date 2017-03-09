8275102f29076e235362a98ff5a44fd84a2dcf3e
Serdiuk Roman <serdiuk.r@gmail.com>
2017-03-09 22:11:42 +0200

    edit secret key

--- django_demo/settings.py
+++ django_demo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from . import secret_key
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^y7mhz-tx*_yk9(a1r+gn=%k&lwk_wkg2@er7adtyquo05(jc1'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
