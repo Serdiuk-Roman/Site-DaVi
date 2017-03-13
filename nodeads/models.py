1dc337199551cd79192dd99acdd1a31c26b1cae8
Serdiuk Roman <serdiuk.r@gmail.com>
2017-03-13 17:52:25 +0200

    edit weather models & view, float & int

--- nodeads/models.py
+++ nodeads/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
 class Culeba(models.Model):
-    name = models.CharField(max_length=140)
-    lon = models.CharField(max_length=140)
-    lat = models.CharField(max_length=140)
+    name = models.CharField(max_length=50)
+    lon = models.FloatField()
+    lat = models.FloatField()
+
     date = models.CharField(max_length=140)
     time = models.CharField(max_length=140)
-    temp_max = models.CharField(max_length=140)
-    temp_min = models.CharField(max_length=140)
-    wind = models.CharField(max_length=140)
-    cloud = models.CharField(max_length=140)
-    pressure = models.CharField(max_length=140)
+
+    temp_max = models.FloatField()
+    temp_min = models.FloatField()
+
+    wind = models.FloatField()
+    cloud = models.IntegerField()
+    pressure = models.FloatField()
     description = models.CharField(max_length=140)
-    
+
     def __str__(self):
         return str(self.date) + str(' ') + str(self.time)
--- nodeads/views.py
+++ nodeads/views.py
@@ -22,20 +22,20 @@ def ajax(request):
         return JsonResponse({'dt_t' : dt_t, 'temp': temp, 'dt_p' : dt_p, 'pres': pres})
 
 def owmo(request):
-    
+
     d = datetime.date.today()
     x = Culeba.objects.filter(date__gte = d)
-    
+
     #little table
-    
+
     date_lt = sorted(set([i.date for i in x]))
 
     #time_lt = sorted(set([j.time for j in x]))
-    
+
     little_table = [[x] for x in date_lt]
     counter = 0
     for j in date_lt:
-        
+
         db_lt = Culeba.objects.filter(date = j)
         little_table[counter].append(max([float(k.temp_max) for k in db_lt]))
         little_table[counter].append(min([float(k.temp_min) for k in db_lt]))
@@ -49,9 +49,9 @@ def owmo(request):
         n2 = [i[0] for i in collections.Counter(n).most_common(1)]
         little_table[counter].append(n2[0])
         counter += 1
-    
+
     #big table
-    
+
     city_name = [x[0].name, x[0].lon, x[0].lat]
     step = sorted(set([i.date for i in x]))
     for_table = [[i.date, i.time[:-3], i.temp_max, i.temp_min, i.wind, i.cloud, i.pressure, i.description] for i in x]
@@ -72,13 +72,13 @@ def owm(request):
         x = Culeba.objects.filter(date = c.date).filter(time = c.time)
         if not x:
             c.name = forecast['city']['name']
-            c.lon = forecast['city']['coord']['lon']
-            c.lat = forecast['city']['coord']['lat']
-            c.temp_max = i['main']['temp_max']
-            c.temp_min = i['main']['temp_min']
-            c.wind = i['wind']['speed']
-            c.cloud = i['clouds']['all']
-            c.pressure = i['main']['pressure']
+            c.lon = float(forecast['city']['coord']['lon'])
+            c.lat = float(forecast['city']['coord']['lat'])
+            c.temp_max = float(i['main']['temp_max'])
+            c.temp_min = float(i['main']['temp_min'])
+            c.wind = float(i['wind']['speed'])
+            c.cloud = int(i['clouds']['all'])
+            c.pressure = float(i['main']['pressure'])
             c.description = i['weather'][0]['description']
             c.save()                
         x = None
